00c01fc44a2436e88e28a844326ef06f988916e9
wouter <wouter@lagerweij.com>
2015-04-26 01:14:30 +0200

    docker build script

---
+++ build_docker_petclinic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "building petclinic"
+mvn clean install
+
+echo "Copying docker files"
+cp docker/Dockerfile target/
+
+echo "Building Docker image"
+cd target/
+docker build -t wouterla/docker-petclinic .
+
+echo "Pushing docker image to repository"
+#docker push wouterla/docker-jenkins
